a74ad6cb984bf2320662918d7a39cca179f24d70
Cyborg231 <lubieolejj@gmail.com>
2026-04-23 18:22:27 +0200

    Zadanie 3 - Pogodynka (gotowe)

--- lab_3/lab_3/Program.cs
+++ lab_3/lab_3/Program.cs
@@ -1,17 +1,20 @@
+using lab_3.Prezenter;
+
 namespace lab_3
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 view = new Form1();
+            var model = new lab_3.Modele.WeatherService();
+            var presenter = new WeatherPresenter(view, model);
+
+            Application.Run(view);
         }
     }
 }
